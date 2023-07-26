500634c5d873c67ee069267f7ce38f0a6fc58ba6
kurmakadagalla <kadagallak@gmail.com>
2023-07-26 13:59:02 +0530

    commit1

---
+++ src/COREJAVA/DATATYPES.java
@@ -0,0 +1,18 @@
+package COREJAVA;
+
+public class DATATYPES {
+
+	public static void main(String[] args) {
+	
+		int a=100;
+		
+		float b=100.5f;
+		
+		double c=100.79;
+		
+		System.out.println(c);
+		
+		System.out.println(b);
+	}
+
+}
